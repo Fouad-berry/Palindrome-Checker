22d37ef5e7cef2b1993667420ee38cc572af150f
Fouad-berry <fidriss96@gmail.com>
2024-10-27 11:35:22 +0100

    remove ponctuation and space on the verification

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/* const txtInput = document.querySelector("input");
-const checkBtn = document.querySelector(".inputs button");
-const infoTxt = document.querySelector(".info-txt");
-
-let userInput = "";
-
-checkBtn.addEventListener("click", checkReverse);
-
-txtInput.addEventListener("keyup", (event) => {
-  if (event.key === "Enter") checkBtn.click();
-
-  if (event.target.value !== "") checkBtn.classList.add("active");
-  else {
-    checkBtn.classList.remove("active");
-    infoTxt.style.display = "none";
-  }
-});
-
-function checkReverse() {
-  userInput = txtInput.value.trim().toLowerCase();
-
-  const reverseInput = userInput.split("").reverse().join("");
-  infoTxt.style.display = "block";
-
-  if (userInput !== reverseInput)
-    infoTxt.innerHTML = `Non, <span> '${userInput}' </span> n'est pas un palindrome!`;
-  else infoTxt.innerHTML = `Oui, <span>'${userInput}'</span> est un palindrome!`;
-}
-
- */
-
 const txtInput = document.querySelector("input");
 const checkBtn = document.querySelector(".inputs button");
 const infoTxt = document.querySelector(".info-txt");
@@ -48,8 +17,8 @@ txtInput.addEventListener("keyup", (event) => {
 });
 
 function checkReverse() {
-  // Supprimer les espaces et convertir en minuscules
-  userInput = txtInput.value.replace(/\s+/g, "").toLowerCase();
+  // Supprimer les espaces, ponctuations et convertir en minuscules
+  userInput = txtInput.value.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
   const reverseInput = userInput.split("").reverse().join("");
   infoTxt.style.display = "block";
