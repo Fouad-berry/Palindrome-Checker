e27b8eb3b8b6eb61d339efbafaa4950a31e0ec96
Fouad-berry <fidriss96@gmail.com>
2024-10-27 11:26:47 +0100

    add gsap animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const txtInput = document.querySelector("input");
+/* const txtInput = document.querySelector("input");
 const checkBtn = document.querySelector(".inputs button");
 const infoTxt = document.querySelector(".info-txt");
 
@@ -27,3 +27,35 @@ function checkReverse() {
   else infoTxt.innerHTML = `Oui, <span>'${userInput}'</span> est un palindrome!`;
 }
 
+ */
+
+const txtInput = document.querySelector("input");
+const checkBtn = document.querySelector(".inputs button");
+const infoTxt = document.querySelector(".info-txt");
+
+let userInput = "";
+
+checkBtn.addEventListener("click", checkReverse);
+
+txtInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") checkBtn.click();
+
+  if (event.target.value !== "") checkBtn.classList.add("active");
+  else {
+    checkBtn.classList.remove("active");
+    infoTxt.style.display = "none";
+  }
+});
+
+function checkReverse() {
+  // Supprimer les espaces et convertir en minuscules
+  userInput = txtInput.value.replace(/\s+/g, "").toLowerCase();
+
+  const reverseInput = userInput.split("").reverse().join("");
+  infoTxt.style.display = "block";
+
+  if (userInput !== reverseInput)
+    infoTxt.innerHTML = `Non, <span> '${txtInput.value}' </span> n'est pas un palindrome!`;
+  else
+    infoTxt.innerHTML = `Oui, <span>'${txtInput.value}'</span> est un palindrome!`;
+}
